90d3a6e29c3fb35ccfeb6721cf9b1073d1b6965e
Natasha Png <natashapng.png@gmail.com>
2025-08-28 00:37:59 +1000

    UI change

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1/20;
 
+
+
+// ...existing code...
+
+
+
+document.addEventListener("mousemove", updateGradient);
+
 function moveBackground(event) {
   const shapes = document.querySelectorAll(".shape");
   const x = event.clientX * scaleFactor;
@@ -27,8 +35,8 @@ function toggleContrast() {
       document.body.classList.remove("dark-theme")
     }
   }
-  
 
+  
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -62,3 +70,19 @@ function toggleModal() {
     document.body.classList += " modal--open";
 }
 
+function updateGradient(event) {
+  const x = event.clientX / window.innerWidth;
+  const y = event.clientY / window.innerHeight;
+
+  const background = document.getElementById("background");
+  background.style.background = `radial-gradient(
+      circle at ${x * 100}% ${y * 100}%, 
+      #fceabb, 
+      transparent 50%
+    ),
+    radial-gradient(
+      circle at ${(1 - x) * 100}% ${(1 - y) * 100}%, 
+#84ddda, 
+      transparent 50%
+    )`;
+}
